8100fe86b9800df8df02f3a659923c4bb360fcd5
leemingee <leemingee1995@gmail.com>
2019-01-17 17:40:59 -0500

    LR added

--- Clustering/DBSCAN.py
+++ Clustering/DBSCAN.py
@@ -7,6 +7,10 @@ Description: DBSCAN from scratch
 
 Contact: ming.li2@columbia.edu
 '''
+from Cluster_min import Cluster
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import scipy
 
 class DBSCAN:
     '''
@@ -15,6 +19,7 @@ class DBSCAN:
     Good for data which contains clusters of similar density.
 
     reference: https://cse.buffalo.edu/~jing/cse601/fa13/materials/clustering_density.pdf
+    https://github.com/SushantKafle/DBSCAN/blob/master/cluster.py
     '''
 
     def __init__(self, eps, minPts, metric = 'euclidean'):
@@ -56,5 +61,30 @@ class DBSCAN:
             return all points within P's eps-neighborhood (including P)
         '''
 
+
+        '''
+        DBSCAN(D, eps, MinPts)
+             C = 0
+             for each unvisited point P in dataset D
+                mark P as visited
+                NeighborPts = regionQuery(P, eps)
+                if sizeof(NeighborPts) < MinPts
+                    mark P as NOISE
+                else
+                    C = next cluster
+                    expandCluster(P, NeighborPts, C, eps, MinPts)
+        '''
+        # initial a noise cluster
+        noise = Cluster('Noise', self.dim)
+        counts = D.shape[0]
+        self.visited = [0] * counts
+        for i in range(counts):
+            point = D[i]
+            self.visited[i] = 1
+            NeighborPts = regionQuery(point, self.eps)
+            if len(NeighborPts) < self.MinPts:
+                clusters[i] = -1 # mark as noise
+            else:
+                # TODO finish this one
 
 # TODO read about this https://github.com/bwoneill/pypardis the dbscan on pyspark
--- readme.md
+++ readme.md
@@ -3,3 +3,33 @@ Where I implement the ML from scratch using python.
 I expect to implement the simpler and reduced version of the algorithms in a agile update iterations.
 
 Start from 01/17/19, mainly for preparation for the interview at XXXX.
+
+
+## Directory<a name="directory"></a>
+- [Algorithm list](#algorithm-list)
+  - [Classify](#classify)
+    - Perceptron
+    - K Nearest Neightbor (KNN)
+    - Naive Bayes
+    - Decision Tree
+    - Random Forest
+    - SVM
+    - AdaBoost
+    - HMM
+  - [Cluster](#Clustering)
+    - KMeans
+    - Affinity Propagation
+  - [Manifold Learning](#manifold-learning)
+    - PCA
+    - Locally-linear-embedding (LLE)
+  - [NLP](#nlp)
+    - LDA
+  - [Time Series Analysis](#time-series-analysis)
+    - AR
+- [Usage](#usage)
+  - Installation
+  - Examples for *Statistical Learning Method*(《统计学习方法》)
+- [Reference](#reference)
+## Algorithm List<a name="algorithm-list"></a>
+### Classify<a name="classify"></a>
+- Perceptron
